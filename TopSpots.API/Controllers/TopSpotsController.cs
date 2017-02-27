349d03f10d30ba3970d54f8bfca29d9c6c3f5845
ch3rr17 <cherr.batac@gmail.com>
2017-02-27 15:04:46 -0800

    CRUD METHOD: GET AND POST WORKS, WORKING ON PUT AND DELETE IIS restrictions

--- TopSpots.API/Controllers/TopSpotsController.cs
+++ TopSpots.API/Controllers/TopSpotsController.cs
@@ -10,6 +10,7 @@ using System.Web.Http;
 using System.Web.Http.Cors;
 using TopSpots.API.Models;
 
+
 namespace TopSpots.API.Controllers
 {
     [EnableCors("*", "*", "*")]
@@ -18,12 +19,11 @@ namespace TopSpots.API.Controllers
         // GET: api/TopSpots
         public IEnumerable<TopSpot> Get()
         {
-
             string json = File.ReadAllText("C:/Users/cherr/dev/TopSpots/TopSpots.API/TopSpots.API/topspots.json");
             var output = JsonConvert.DeserializeObject<List<TopSpot>>(json);
             return output;
-
         }
+
 
         // GET: api/TopSpots/5
         public string Get(int id)
@@ -34,23 +34,71 @@ namespace TopSpots.API.Controllers
         // POST: api/TopSpots
         public TopSpot Post(TopSpot spot)
         {
-            
+            string json = File.ReadAllText("C:/Users/cherr/dev/TopSpots/TopSpots.API/TopSpots.API/topspots.json");
+            var output = JsonConvert.DeserializeObject<List<TopSpot>>(json);
+
+            output.Add(spot);
+
+            string newSpot = Newtonsoft.Json.JsonConvert.SerializeObject(output);
+            File.WriteAllText("C:/Users/cherr/dev/TopSpots/TopSpots.API/TopSpots.API/topspots.json", newSpot);
+
+            return spot;
         }
+
 
         // PUT: api/TopSpots/5
-        public void Put(int id, TopSpot spot)
+        [HttpPut]
+        public HttpResponseMessage Put(int id, TopSpot spot)
         {
-           
+            string json = File.ReadAllText("C:/Users/cherr/dev/TopSpots/TopSpots.API/TopSpots.API/topspots.json");
+            var output = JsonConvert.DeserializeObject<List<TopSpot>>(json);
 
+            if(spot.Name != null)
+            {
+                output[id].Name = spot.Name;
+            }
+
+            if(spot.Description != null)
+            {
+                output[id].Description = spot.Description;
+            }
+
+            if (spot.Location != null)
+            {
+                output[id].Location[0] = spot.Location[0];
+                output[id].Location[1] = spot.Location[1];
+            }
+
+            string newSpot = Newtonsoft.Json.JsonConvert.SerializeObject(output);
+            File.WriteAllText("C:/Users/cherr/dev/TopSpots/TopSpots.API/TopSpots.API/topspots.json", newSpot);
+
+            return Request.CreateResponse(HttpStatusCode.OK,
+            new
+            {
+                value = spot,
+                message = "success!"
+            });
 
         }
 
+        
         // DELETE: api/TopSpots/5
-        public void Delete(int id, TopSpot spot)
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
         {
-            
+            string json = File.ReadAllText("C:/Users/cherr/dev/TopSpots/TopSpots.API/TopSpots.API/topspots.json");
+            var output = JsonConvert.DeserializeObject<List<TopSpot>>(json);
 
+            output.RemoveAt(id);
 
+            string spots = Newtonsoft.Json.JsonConvert.SerializeObject(output);
+            File.WriteAllText("C:/Users/cherr/dev/TopSpots/TopSpots.API/TopSpots.API/topspots.json", spots);
+
+            return Request.CreateResponse(HttpStatusCode.OK,
+            new
+            {
+                message = "success!"
+            });
 
         }
     }
